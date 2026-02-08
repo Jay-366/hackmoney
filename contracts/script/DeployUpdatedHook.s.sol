690939a346733203e8e6e1c7aa7b2e7c664ef405
Lim Fang Yee <fangyee0304@gmail.com>
2026-02-08 19:24:23 +0800

    test token with hoooks

--- contracts/script/DeployUpdatedHook.s.sol
+++ contracts/script/DeployUpdatedHook.s.sol
@@ -1,28 +1,25 @@
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-
 import {AminoRiskFeeHook} from "../src/AminoRiskFeeHook.sol";
 import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
 import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
 import {HookMiner} from "../lib/v4-periphery/src/utils/HookMiner.sol";
 
-contract DeployAminoRiskFeeHookScript is Script {
-    // NOTE: ensure this PoolManager is the one your pool uses.
-    // If you earlier used 0x429b..., replace the value below with that.
+contract DeployUpdatedHookScript is Script {
     address constant SEPOLIA_POOL_MANAGER = 0xE03A1074c86CFeDd5C142C4F04F1a1536e203543;
     address constant CREATE2_DEPLOYER = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
+    
+    // User provided Reputation Registry
+    address constant REP_REGISTRY = 0x3bb25E47ADA8527C264c582f7763b6e5C2a8E2a6;
 
     function run() external returns (AminoRiskFeeHook hook) {
-        // The owner for the hook (who can call setRegistry etc)
         address deployerAddress = vm.envAddress("DEPLOYER_ADDRESS");
 
-        // mining flags: BEFORE_SWAP + AFTER_SWAP
         uint160 flags = uint160(Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG);
 
-        console2.log("Miner flags:", flags);
-        // NOTE: encode constructor args (IPoolManager, owner)
         (address hookAddress, bytes32 salt) = HookMiner.find(
             CREATE2_DEPLOYER,
             flags,
@@ -32,13 +29,16 @@ contract DeployAminoRiskFeeHookScript is Script {
 
         vm.startBroadcast();
 
-        // Deploy with same constructor args you encoded above
         hook = new AminoRiskFeeHook{salt: salt}(IPoolManager(SEPOLIA_POOL_MANAGER), deployerAddress);
-
+        
         require(address(hook) == hookAddress, "Hook address mismatch");
-
+        
+        // Set Registry
+        hook.setRegistry(REP_REGISTRY); // Points to Reputation Registry
+        
         vm.stopBroadcast();
 
-        console2.log("AminoRiskFeeHook deployed at:", address(hook));
+        console2.log("Updated AminoRiskFeeHook deployed at:", address(hook));
+        console2.log("Registry set to:", REP_REGISTRY);
     }
 }
--- contracts/src/MockERC20.sol
+++ contracts/src/MockERC20.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.26;
 contract MockERC20 {
     string public name;
     string public symbol;
-    uint8 public immutable decimals = 18;
+    function decimals() external pure returns (uint8) {
+        return 18;
+    }
     uint256 public totalSupply;
 
     mapping(address => uint256) public balanceOf;
